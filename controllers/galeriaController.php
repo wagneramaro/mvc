7c5b21461988caa71042e2207b619b3693026b96
Wagner Amaro (Notebook) <contato@agenciawide.com.br>
2020-03-01 19:02:37 -0300

    exemplos de controller

---
+++ controllers/galeriaController.php
@@ -0,0 +1,10 @@
+<?php 
+class galeriaController {
+    public function index() {
+        echo "Listando todas as galerias";
+    }
+
+    public function abrir($id){
+        echo "abrindo galeria ".$id;
+    }
+}
